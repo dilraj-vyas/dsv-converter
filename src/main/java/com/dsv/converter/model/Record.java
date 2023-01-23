1e92128a86a76420a9df273b17de6765509bc44b
hello vyas <dilrajblore@gmail.com>
2023-01-23 23:53:57 +0530

    adding java doc

--- src/main/java/com/dsv/converter/model/Record.java
+++ src/main/java/com/dsv/converter/model/Record.java
@@ -11,6 +11,13 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
+/**
+ * The Record class represents a record in a CSV file. It contains a map of column names to values,
+ * and provides methods for getting and setting the values of individual columns.
+ *
+ * @Data annotation is used to generate all the getters, setters, equals, hashCode, and toString methods
+ */
+
 @Data
 @ToString
 @NoArgsConstructor
